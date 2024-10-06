34241b898c57a8a9ff1291267c6756bc745ea262
takahashiitsuki <itakaha71@gmail.com>
2024-10-06 13:29:35 +0900

    [Finish]character_database

--- character_database/MainWindow.xaml.cs
+++ character_database/MainWindow.xaml.cs
@@ -9,6 +9,19 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// キャラクター情報を格納するクラスを定義
+public class Character
+{
+    public string Name { get; set; }
+    public int HP { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} - HP: {HP}";
+    }
+}
+
+
 namespace character_database
 {
     /// <summary>
@@ -20,5 +33,36 @@ namespace character_database
         {
             InitializeComponent();
         }
+
+        // キャラクターリストを格納する
+        private List<Character> characters = new List<Character>();
+
+        // キャラクターを追加するボタンクリックイベント
+        private void AddCharacter_Click(object sender, RoutedEventArgs e)
+        {
+            // 入力された名前とHPを取得
+            string name = characterNameTextBox.Text;
+            int hp;
+
+            // HPが数値であるか確認し、キャラクターをリストに追加
+            if (int.TryParse(characterHPTextBox.Text, out hp))
+            {
+                Character newCharacter = new Character { Name = name, HP = hp };
+                characters.Add(newCharacter);
+
+                // リストボックスに表示を更新
+                characterListBox.Items.Add(newCharacter);
+
+                // テキストボックスをクリア
+                characterNameTextBox.Clear();
+                characterHPTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("HPは数値を入力してください。");
+            }
+        }
+
+
+        }
     }
-}
